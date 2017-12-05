5001230a5b96322ed5f586dfc3dd56278bedfd5c
adascalu <alexandru.dascalu@adswizz.com>
2017-12-05 10:07:16 +0200

    Ex20.21

---
+++ src/main/java/ch30/ParallelMergeSort.java
@@ -0,0 +1,64 @@
+package ch30;
+
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class ParallelMergeSort {
+    public static void main(String[] args) {
+        final int SIZE = 7_000_000;
+        int[] list1 = new int[SIZE];
+        int[] list2 = new int[SIZE];
+
+        for (int i = 0; i < list1.length; i++)
+            list1[1] = list2[i] = (int) (Math.random() * 10_000_000);
+        long startTime = System.currentTimeMillis();
+        parallelMergeSort(list1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Parallel time with: " + Runtime.getRuntime().availableProcessors() +
+                " processors is: " + (endTime - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        MergeSort.mergeSort(list2);
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Sequential time with: " + Runtime.getRuntime().availableProcessors() +
+                " processors is: " + (endTime - startTime) + " ms");
+    }
+
+    private static void parallelMergeSort(int[] list) {
+
+        RecursiveAction mainTask = new SortTask(list);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(mainTask);
+
+    }
+
+    private static class SortTask extends RecursiveAction {
+
+        private final int THRESHOLD = 500;
+        private int[] list;
+
+        public SortTask(int[] list) {
+            this.list = list;
+        }
+
+        @Override
+        protected void compute() {
+            if (list.length < THRESHOLD)
+                java.util.Arrays.sort(list);
+            else {
+
+                int[] firstHalf = new int[list.length / 2];
+                System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+
+                int secondHalfLength = list.length - list.length / 2;
+                int[] secondHalf = new int[secondHalfLength];
+                System.arraycopy(list, list.length / 2, secondHalf, 0, secondHalfLength);
+
+                invokeAll(new SortTask(firstHalf), new SortTask(secondHalf));
+                MergeSort.merge(firstHalf, secondHalf, list);
+            }
+        }
+    }
+}
